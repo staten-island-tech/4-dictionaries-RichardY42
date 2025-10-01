d7591a509167b6b8442f056b2cb091b23b39cf30
RichardY42 <RichardY42@nycstudents.net>
2025-10-01 10:19:49 -0400

    brisk 10/1 1019a

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,18 @@
-mart={
+drinks={
     "name": "Brisk",
     "price": 2.50,
     "type": "Iced Tea"
 }
+chips={
+    "name": "Pop Corners",
+    "price": 3.00,
+    "type": "chips"
+}
+gum={
+    "name": "Watermelon gum",
+    "price": 2.00,
+    "type": "gum"
+}
+result=input("welcome, would you like to buy drinks, chips, or gum? ")
+if result=="drinks":
+    print(drinks["name"])
