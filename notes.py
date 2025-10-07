a1256ad549b380a6ba477bd80a7f7f28398ee8fe
RichardY42 <RichardY42@nycstudents.net>
2025-10-07 10:16:49 -0400

    occu spc

---
+++ notes.py
@@ -0,0 +1,20 @@
+#eng or fren assign., should be put in a def func, doesnt need input func
+def lang(x):
+    s=0
+    t=0
+    for char in x:
+        if char == "" or char=="":
+            s+=1
+        elif char =="" or char== "":
+            t+=1
+        if s>t:
+            print
+        if t<s:
+            print
+
+
+lang("asgdkelfghsssfttttdtdtdstststtssfiueawhftti")
+#occupy space, should not have to use input func
+spaces(y,t):
+
+space("cccc.c.","c.c.c.")
--- testprep.py
+++ testprep.py
@@ -1,13 +1,25 @@
-x=input("type in a sentence i determine if it is english or french: ")
-eng=[]
-fren=[]
-for ltr in x:
-    if ltr == "t" or ltr =="T":
-        eng.append(ltr)
-    if ltr=="s" or ltr == "S":
-        fren.append(ltr)
-if eng>fren:
-    print("english")
-elif eng<fren:
-    print("french")
+#Eng or Fren determiner
+"""def langd(x):
+    eng=0
+    fren=0
+    for ltr in x:
+        if ltr == "t" or ltr =="T":
+            eng+=1
+        if ltr=="s" or ltr == "S":
+            fren+=1
+    if eng>fren:
+        print("english")
+    elif eng<fren:
+        print("french")
 #print(eng)
+#print(fren)
+langd("Lorsque j'avais six ans j'ai vu, une fois,")"""
+#Occupy Spaces 
+
+def idk(y,t):
+    occuty=0
+    for taken in y:
+        if taken in y==taken in t:
+            occuty+=1
+    print(occuty)
+idk(".T.TT.","T.")
